c6d414e61f60d49f12c41bc4a73e2e00c30b966e
Vaibhav Sharma <vaibhav.sharma@revv.co.in>
2021-07-20 20:31:51 +0530

    Section 4 - Chapter 15

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,14 @@
 const chalk = require('chalk');
 
-console.log(chalk.green.bold.inverse("success"))
+console.log(chalk.green.bold.inverse("dfdf"));
+
+console.log(process.argv)
+
+const command = process.argv[2];
+
+if(command === 'add'){
+    console.log('adding note');
+}
+else if(command == 'remove'){
+    console.log('removing note');
+}
