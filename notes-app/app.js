1c133fff4fc58926ac360ddd2677489e034b1606
Vaibhav Sharma <vaibhav.sharma@revv.co.in>
2021-07-21 22:20:31 +0530

    Section 3 - Chapter 16 - Using Yargs to Set up commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,42 @@
+const yargs = require('yargs');
 const chalk = require('chalk');
 
-console.log(chalk.green.bold.inverse("dfdf"));
 
-console.log(process.argv)
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    handler: ()=>{
+        console.log("adding a new note")
+    }
+})
 
-const command = process.argv[2];
+yargs.command({
+    command:'remove',
+    describe:'Remove a note',
+    handler: ()=>{
+        console.log("removing a note")
+    }
+})
 
-if(command === 'add'){
-    console.log('adding note');
-}
-else if(command == 'remove'){
-    console.log('removing note');
-}
+
+yargs.command({
+    command:'list',
+    describe:'listing notes',
+    handler: ()=>{
+        console.log("listing notes")
+    }
+})
+
+
+yargs.command({
+    command:'read',
+    describe:'reading the notes',
+    handler: ()=>{
+        console.log("reading the notes")
+    }
+})
+
+
+
+
+console.log(yargs.argv)
