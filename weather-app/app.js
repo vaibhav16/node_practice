624e47fc0633dca50e1739b646c224501c414730
vaibhav16 <vssharma.vaibhav5@gmail.com>
2021-07-31 11:53:24 +0530

    Section 6 - Chapter 29 - Asynchronous JS

---
+++ weather-app/app.js
@@ -0,0 +1,13 @@
+console.log('Starting');
+
+setTimeout(() => {
+    console.log("2 Second Timeout");    
+}, 2000);
+
+setTimeout(() => {
+    console.log("0 second Timeout");
+}, 0);
+
+
+console.log('Stopping');
+
