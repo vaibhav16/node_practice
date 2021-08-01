da24f4a600243c83a27d8f3441cb277e9f2fcbe1
vaibhav16 <vssharma.vaibhav5@gmail.com>
2021-08-01 22:50:37 +0530

    Just keep Codinggit add .!

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,5 @@
 const request = require('postman-request');
-
+//APIKEY=a87d7844bdfe1cc96e40e1d40e6c096e
 const url = "http://api.weatherstack.com/current?access_key=a87d7844bdfe1cc96e40e1d40e6c096e&query=37.8267,-122.4233";
 
 request(url, (error, response, body) => {
