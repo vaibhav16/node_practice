838ad66bbd92ea1fc56cbb44e9ed56180806e91c
vaibhav16 <vssharma.vaibhav5@gmail.com>
2021-08-05 20:27:07 +0530

    Section 6 - Chapter 32 - Using Weatherstack API

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,10 @@
 const request = require('postman-request');
 //APIKEY=a87d7844bdfe1cc96e40e1d40e6c096e
-const url = "http://api.weatherstack.com/current?access_key=a87d7844bdfe1cc96e40e1d40e6c096e&query=37.8267,-122.4233";
+const url = "http://api.weatherstack.com/current?access_key=a87d7844bdfe1cc96e40e1d40e6c096e&query=37.8267,-122.4233&units=f";
 
-request(url, (error, response, body) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({url:url,json:'json'}, (error, response, body) => {
+    //console.log(response);
+    const data = response.body.current;
+    const str = data.weather_descriptions[0] + ". It is currently "+data.temperature+" degrees out. It feels like "+ data.feelslike +" degrees out."
+    console.log(str);
 })
