0caa8190cb612b17722e8fe3371679944d7b8497
vaibhav16 <vssharma.vaibhav5@gmail.com>
2021-08-12 20:07:11 +0530

    Section  7 - Chapter 44 - Serving HTML and JSON through express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,19 +4,32 @@ const app = express();
 
 
 app.get('',(req,res)=>{
-    res.send('Hello Express!!!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help',(req,res)=>{
-    res.send('Help Page')
+    res.send([{
+        name:'Bond',
+        age:44
+
+    },
+    {
+        name:'Deadpool',
+        age:99
+
+    }
+])
 })
 
 app.get('/about',(req,res)=>{
-    res.send("About Page!")
+    res.send("<h1>About Page!</h1>")
 })
 
 app.get('/weather',(req,res)=>{
-    res.send('Weather Page');
+    res.send({
+        forecast:'Its going to be sunny.',
+        location:'London'
+    });
 })
 
 //app.com
